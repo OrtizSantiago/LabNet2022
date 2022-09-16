f881a63b649ed81a7d75b1c26452470c8da97dd4
Santiago Ortiz <unaj.santiago.ortiz@gmail.com>
2022-09-16 00:28:49 -0300

    Segundo Cambio Consola de Transporte

---
+++ Unidad02/Unidad02/ConsolaTransporte/Interfaces/IOperaciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaTransporte.Interfaces
+{
+    public interface IOperaciones<T> where T : class
+    {
+        void Agregar(T entidad);
+        string Mostrar();
+    }
+
+
+
+
+}
---
+++ Unidad02/Unidad02/ConsolaTransporte/Logica/LogicaTaxi.cs
@@ -0,0 +1,34 @@
+﻿using ConsolaTransporte.Interfaces;
+using ConsolaTransporte.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaTransporte.Logica
+{
+    public class LogicaTaxi : IOperaciones<Taxi>
+    {
+        List<Taxi> ListaTaxi = new List<Taxi>();
+        public void Agregar(Taxi entidad)
+        {
+            if (entidad != null)
+            {
+                ListaTaxi.Add(entidad);
+            }
+        }
+
+        public string Mostrar()
+        {
+            string text = "Lista de Taxis \n";
+            int n = 1;
+            foreach (Taxi i in ListaTaxi)
+            {
+                text += "Taxi #" + n + ": " + i.Pasajeros + " pasajeros. \n";
+                n++;
+            }
+            return text;
+        }
+    }
+}
